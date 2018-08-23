99d7ab54b31bcf507ce004c03542852f21f0f8f9
Victor Maldonado <victor.maldonado@rappi.com>
2018-08-23 14:00:11 -0500

    Simple sum.

--- week1/main.rb
+++ week1/main.rb
@@ -3,6 +3,15 @@ def reverseArray(a)
   a.reverse
 end
 
+def simpleArraySum(ar)
+  sum = 0
+  for x in ar do
+    sum = sum + x
+  end
+  sum
+end
+
 if __FILE__ == $0
   puts reverseArray([1,2,3])
+  puts simpleArraySum([1,2,3])
 end
